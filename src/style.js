8e5800f3592dd334cdf508f84d0f7c505adb07e8
Raymond9734 <rymadara97@gmail.com>
2024-10-27 12:59:44 +0300

    Create reusable style constants
    - Define padding and layout styles
    - Set up text styles for different sections (hero, headings, subtext)
    - Establish color scheme constants

---
+++ src/style.js
@@ -0,0 +1,31 @@
+const styles = {
+    paddingX: "sm:px-16 px-6",
+    paddingY: "sm:py-16 py-6",
+    padding: "sm:px-16 px-6 sm:py-16 py-10",
+  
+    heroHeadText:
+      "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+    heroSubText:
+      "text-white font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+  
+    sectionHeadText:
+      "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    sectionSubText:
+    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+    bgColor:
+      "#2b2b2b",
+    whiteColor:
+      "white",
+    subDarkTxtColor:
+      "#2b2b2b",
+    subLightColor:
+      "#8ddd8d",
+    darkBgColor:
+      "#131313",
+    arrowBlackColor:
+      "black",
+    hoverDarkWhiteTxtColor:
+      "#727272",
+  };
+  
+  export { styles };
