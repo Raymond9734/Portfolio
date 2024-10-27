54ae2fc949ac23d8f81db26e980474e353fcb615
Raymond9734 <rymadara97@gmail.com>
2024-10-27 13:00:42 +0300

    Add assets required for the project

---
+++ src/assets/index.js
@@ -0,0 +1,79 @@
+import logo from "./logo1.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";  
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+
+import zone01 from "./company/zone01Logo.png";
+import aws from "./tech/aws-logo.jpg";
+import django from "./tech/django-basics.png";
+import python from "./tech/Python-logo.png";
+import postgres from "./tech/Postgres-logo.png";
+import bootstrap from "./tech/bootstrap-logo.png";
+import cLogo from "./tech/c-logo.png";
+import golang from "./tech/golang-logo.png";
+import cPlusPlus from "./tech/C++-logo.png";
+import mysql from "./tech/MySQL-logo.png";
+import vite from "./tech/vite-logo.png";
+import groupie from "./Projects/groupie-project.png";
+import netfix from "./Projects/netfix-project.png";
+import systemMonitor from "./Projects/system-monitor-project.png";
+import terminalUi from "./Projects/terminal-ui.png";
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  zone01,
+  aws,
+  django,
+  python,
+  postgres,
+  bootstrap,
+  cLogo,
+  golang,
+  cPlusPlus,
+  mysql,
+  vite,
+  groupie,
+  netfix,
+  systemMonitor,    
+  terminalUi,
+};
