2f87b6a960b037649477db78aeaff8328989ab75
Raymond9734 <rymadara97@gmail.com>
2025-07-24 05:51:29 +0300

    Update: Refactor import statement in Contact component to improve readability

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import { useState, useRef } from 'react'
 import { motion } from 'framer-motion'
 import { styles } from '../style'
 import { EarthCanvas } from './canvas'
